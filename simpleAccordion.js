8f5f169cd35bae8bfe5be5f9ecdbc5db4478f00c
Christian César Robledo López <osiris2012@msn.com>
2014-12-03 12:21:32 -0600

    Update for smaller size.

--- simpleAccordion.js
+++ simpleAccordion.js
@@ -1,15 +1,23 @@
-(function ($) { //Not using "use strict", but the rules are implemented
-    $.fn.simpleAccordion = function (c,s,t) {
-			c = 'simpleAccordionactive'; s = !0; t = this;
-			$.each(arguments, function(a,b){b.big ? c = b : s = b;});
-			function r(a){a.removeClass(c).next().slideUp();}
-			r(t);
-			return t.each(function (a, b) {
-				b = $(b).on('click', function () {
-					a = b.hasClass(c);
-					s ? r(t) : r(b);
-					if (a) b.addClass(c).next().slideDown();
-				});
+(function ($) {//Not using "use strict", but the rules are implemented
+	var f = 'simpleAccordion';
+    $.fn[f] = function () {
+		var c = f+'active',
+			s = !0,
+			t = this;
+		$.each(arguments, function(a,b){
+			b.big ? c = b : s = b;
+		});
+		function r(a){
+			a.removeClass(c).next().slideUp();
+		}
+		r(t);
+		return t.each(function (a, b) {
+			b = $(b);
+			b.on('click', function () {
+				a = b.hasClass(c);
+				s ? r(t) : r(b);
+				if (!a) b.addClass(c).next().slideDown();
 			});
+		});
 	};
 })(jQuery);
